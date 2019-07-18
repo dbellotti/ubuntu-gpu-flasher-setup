f2fba05902cf7e44e79b2971f254a8e13b4c51c8
David Bellotti <dbellotti@users.noreply.github.com>
2019-07-18 16:38:16 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt -y install openssh-server
+sudo apt -y install git
+
+open https://github.com/czombos/AMDGPU-XMR-ETH-Mining-Ubuntu/blob/master/atiflash-linux/atiflash
+mv ~/Downloads/atiflash ./
+chmod +x atiflash
+sudo cp ./atiflash /usr/local/bin
+sudo ./atiflash -s 0 backupbios.rom
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+sudo apt -y install mono-devel
+git clone https://github.com/vanities/PolarisBiosEditor-1.6.7 && cd PolarisBiosEditor-1.6.7
+sudo ./run.sh
